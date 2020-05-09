4ffaafa90c90f0e22bd472766fb3fc24f35d4d62
amruthsrepo <toamruth@gmail.com>
2020-05-09 10:22:54 +0530

    init centers update

--- Excersices/machine-learning-ex7/ex7/kMeansInitCentroids.m
+++ Excersices/machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -1,12 +1,12 @@
 function centroids = kMeansInitCentroids(X, K)
-%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be
 %used in K-Means on the dataset X
 %   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
 %   used with the K-Means on the dataset X
 %
 
 % You should return this values correctly
-centroids = zeros(K, size(X, 2));
+% centroids = zeros(K, size(X, 2));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should set centroids to randomly chosen examples from
@@ -14,7 +14,7 @@ centroids = zeros(K, size(X, 2));
 %
 
 
-
+centroids = X((ceil(rand(K)(:,1) .* size(X, 1))),:);
 
 
 
@@ -23,4 +23,3 @@ centroids = zeros(K, size(X, 2));
 % =============================================================
 
 end
-
